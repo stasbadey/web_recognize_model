5e11dde4712fc695ffc6590643a6c6ecf23143fe
stasbadey <stas5511665@gmail.com>
2023-11-01 18:31:35 +0300

    new changes

--- home_page/views.py
+++ home_page/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from keras.models import load_model
+import cv2
 
 # Create your views here.
+def home_page(request):
+    # model = load_model()
+    # img = cv2.imread()
+    return render(request, 'home_page/home_page.html')
+
+def model(request):
+    return render(request, 'home_page/model.html')
