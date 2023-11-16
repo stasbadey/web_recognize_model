f499e966af6deeea54a019a2f0a6b14e6c725dc8
stasbadey <stas5511665@gmail.com>
2023-11-16 22:40:32 +0300

    update html file, update views

--- home_page/views.py
+++ home_page/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import render
 from keras.models import load_model
+import os
 import cv2
 
 
 # Create your views here.
 def home_page(request):
-    # model = load_model()
+
     # img = cv2.imread()
     return render(request, 'home_page/home_page.html')
 
 
 def model(request):
+    model = load_model(os.path.join('models', 'anotherclassifier.h5'))
     return render(request, 'home_page/model.html')
