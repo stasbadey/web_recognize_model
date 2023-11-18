8592234e51db354f60976fb8800bf446b88eba73
stasbadey <stas5511665@gmail.com>
2023-11-18 08:34:55 +0300

    final version of project without db

--- home_page/urls.py
+++ home_page/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 app_name = 'home_page'
@@ -6,4 +6,5 @@ app_name = 'home_page'
 urlpatterns = [
     path('', views.home_page, name='home_page'),
     path('model/', views.model, name='model'),
+    path('model/classify/', views.classify_image, name='classify_image'),
 ]
--- home_page/views.py
+++ home_page/views.py
@@ -1,16 +1,27 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 from keras.models import load_model
 import os
 import cv2
-
+import numpy as np
+import tensorflow as tf
 
 # Create your views here.
 def home_page(request):
-
-    # img = cv2.imread()
     return render(request, 'home_page/home_page.html')
 
-
 def model(request):
-    model = load_model(os.path.join('models', 'anotherclassifier.h5'))
     return render(request, 'home_page/model.html')
+
+def classify_image(request):
+    if request.method == 'POST' and request.FILES['photo']:
+        image = request.FILES['photo']
+        model = load_model(os.path.join('models', 'anotherclassifier.h5'))
+        img = cv2.imdecode(np.frombuffer(image.read(), np.uint8), cv2.IMREAD_COLOR)
+        resize = cv2.resize(img, (256, 256))
+        yhat = model.predict(np.expand_dims(resize / 255, 0))
+        predicted_class = 'Normal' if yhat[0][0] > 0.7 else 'Defected'
+        print(yhat[0][0])
+        return JsonResponse({'predicted_class': predicted_class})
+    else:
+        return JsonResponse({'error': 'Image file is missing or invalid.'})
